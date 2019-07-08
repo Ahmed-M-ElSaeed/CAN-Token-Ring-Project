6682cc6c6b5e463f8c34751a0013b5af206b0df1
Ahmed-M-ElSaeed <52664182+Ahmed-M-ElSaeed@users.noreply.github.com>
2019-07-08 18:05:25 +0200

    Add files via upload

---
+++ CAN Node 2/config.h
@@ -0,0 +1,26 @@
+/*
+ * config.h
+ *
+ *  Created on: Jul 4, 2019
+ *      Author: AVE-LAP-064
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_can.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/can.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+
+#endif /* CONFIG_H_ */
